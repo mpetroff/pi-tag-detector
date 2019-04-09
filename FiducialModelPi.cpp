44b9e362410fab26df560fa7152c76079c4039bd
Sandino Morales <sandino@carbon.ai>
2019-04-09 14:25:33 -0500

    Fix include bug in FiducialModelPi.cpp

--- FiducialModelPi.cpp
+++ FiducialModelPi.cpp
@@ -2,7 +2,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include "ellipserefine.cpp"
+#include "ellipserefine.hpp"
 
 using namespace ipa_Fiducials;
 
